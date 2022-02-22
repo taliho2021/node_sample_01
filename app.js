4d56eccc0ee9d901a49cf96146a6f3b0ee4bc16f
Tal Yo <tal@analink.com>
2022-02-22 06:30:49 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port =  3000
+
+app.get('/', (req, res) => {
+    res.send('Welcome to ANA Link, Ltd.')
+})
+
+app.listen(port, () => {
+    console.log(`Sample app listening on port ${port}`);
+})
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
